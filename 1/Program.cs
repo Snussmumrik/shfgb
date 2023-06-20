7aa146deae9300808428b53157117530dda4dc14
Marina <PyatkovaM@yandex.ru>
2023-06-20 14:35:16 +0300

    10

---
+++ 1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("Ввведите трехзначное число: ");
+string userInput = Console.ReadLine() ?? "" ;
+int num = int.Parse(userInput);
+
+int a = num % 100;
+
+int result = a / 10;
+
+Console. WriteLine($" Вторая цифра в {num} --> {result} ");
