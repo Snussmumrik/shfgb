531756f0332f207eb98752355dd5be3375e80507
Marina <PyatkovaM@yandex.ru>
2023-06-20 14:40:18 +0300

    15

---
+++ 3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+int NumberA = new Random().Next(1, 8);
+
+if (NumberA == 6 || NumberA == 7)
+{
+Console. WriteLine ($" {NumberA} --> выходной день ");
+}
+else
+{
+Console. WriteLine ($" {NumberA} --> будний день ");
+}
