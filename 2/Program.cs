f703c746f6575f2c0a9a8141af2b512d791ff997
Marina <PyatkovaM@yandex.ru>
2023-06-20 14:38:10 +0300

    13

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int NumberA = new Random().Next(100, 999);
+
+int A = NumberA % 10;
+
+if (NumberA / 100 > 0)
+{
+Console. WriteLine($" Третья цифра в {NumberA} --> {A} ");
+}
+
+else
+{
+Console. WriteLine($" {NumberA} --> двухзначное число ");
+}
